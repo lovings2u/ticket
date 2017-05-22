b9161ae365d021bce356606de4579091f3c95ef8
lovings2u <skyhan1106@gmail.com>
2017-05-22 11:50:32 +0900

    20170522_test

--- app/models/book.rb
+++ app/models/book.rb
@@ -39,54 +39,70 @@ class Book < ApplicationRecord
   end
 
   def self.create_melon_ticket_info
-    uri = 'http://ticket.melon.com/csoon/ajax/listTicketOpen.htm'
-    params = {
-      "orderType" => "0",
-      "pageIndex" => "1",
-      "schText" => ""
-    }
-    headers = {
-      "Accept" => "*/*",
-      "Accept-Encoding" => "gzip, deflate",
-      "Accept-Language" => "ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4",
-      "Connection" => "keep-alive",
-      "Content-Length" => "32",
-      "Content-Type" => "application/x-www-form-urlencoded",
-      "charset" => "UTF-8",
-      "Cookie" => "SCOUTER=z5gppkos3df67t; PCID=14951734126572201067300; WMONID=Df9CR6RuzEU; TKT_POC_ID=MP15",
-      "Host" => "ticket.melon.com",
-      "Origin" => "http://ticket.melon.com",
-      "Referer" => "http://ticket.melon.com/csoon/index.htm",
-      "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-      "X-Requested-With" =>"XMLHttpRequest"
-    }
-    result = RestClient.post uri, params, headers
-    result = Nokogiri::HTML(result)
+    uri = "http://tktapi.melon.com/poc/ticketOpen/list.json?sortType=NEW&pageNo=1&v=1"
+    result = JSON.parse(RestClient.get(uri).body)
 
-    result.css('a.tit').each do |r|
-      href = r['href'].gsub!('./','')
-      uri = "http://ticket.melon.com/csoon/" + href
-      detail = Nokogiri::HTML(open(uri))
-      info = detail.css('div.section_ticketopen_view')
-      puts "#{info.css('p.tit_consert').text}"
+    result["data"]["LIST"].each do |r|
+      info = Nokogiri::HTML(r["infoPerf"])
+      puts r["title"]
       Book.create(
-        concert_name: info.css('p.tit_consert').text,
-        site_url: uri,
-        image_url: info.css('div.box_consert_thumb img')[0]['src'],
-        detail_info: info.css('ul.data_txt').text,
+        concert_name: r["title"],
+        open_date: Time.parse(r["openDt"]),
+        image_url: "http://cdnticket.melon.co.kr#{r["posterUrl"]}",
+        site_url: "http://ticket.melon.com/csoon/detail.htm?csoonId=#{r["csoonId"]}",
+        detail_info: info.text,
         source_site: 2,
-        tc_key: "2-#{uri.split('=').last}"
+        tc_key: "2-#{r['csoonId']}"
       )
     end
-    count = -1
-    book = Book.where(source_site: 2)
-    result.css('span.date').each do |r|
-      next unless r.text.start_with?("2017") & count+=1
-      book[count].update(
-        open_date: Time.parse(r.text)
-      )
-      puts r.text
-    end
+    # uri = 'http://ticket.melon.com/csoon/ajax/listTicketOpen.htm'
+    # params = {
+    #   "orderType" => "0",
+    #   "pageIndex" => "1",
+    #   "schText" => ""
+    # }
+    # headers = {
+    #   "Accept" => "*/*",
+    #   "Accept-Encoding" => "gzip, deflate",
+    #   "Accept-Language" => "ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4",
+    #   "Connection" => "keep-alive",
+    #   "Content-Length" => "32",
+    #   "Content-Type" => "application/x-www-form-urlencoded",
+    #   "charset" => "UTF-8",
+    #   "Cookie" => "SCOUTER=z5gppkos3df67t; PCID=14951734126572201067300; WMONID=Df9CR6RuzEU; TKT_POC_ID=MP15",
+    #   "Host" => "ticket.melon.com",
+    #   "Origin" => "http://ticket.melon.com",
+    #   "Referer" => "http://ticket.melon.com/csoon/index.htm",
+    #   "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    #   "X-Requested-With" =>"XMLHttpRequest"
+    # }
+    # result = RestClient.post uri, params, headers
+    # result = Nokogiri::HTML(result)
+    #
+    # result.css('a.tit').each do |r|
+    #   href = r['href'].gsub!('./','')
+    #   uri = "http://ticket.melon.com/csoon/" + href
+    #   detail = Nokogiri::HTML(open(uri))
+    #   info = detail.css('div.section_ticketopen_view')
+    #   puts "#{info.css('p.tit_consert').text}"
+    #   Book.create(
+    #     concert_name: info.css('p.tit_consert').text,
+    #     site_url: uri,
+    #     image_url: info.css('div.box_consert_thumb img')[0]['src'],
+    #     detail_info: info.css('ul.data_txt').text,
+    #     source_site: 2,
+    #     tc_key: "2-#{uri.split('=').last}"
+    #   )
+    # end
+    # count = -1
+    # book = Book.where(source_site: 2)
+    # result.css('span.date').each do |r|
+    #   next unless r.text.start_with?("2017") & count+=1
+    #   book[count].update(
+    #     open_date: Time.parse(r.text)
+    #   )
+    #   puts r.text
+    # end
   end
 
   def self.create_auction_ticket_info
--- test.rb
+++ test.rb
@@ -1,12 +1,15 @@
 require 'rest_client'
+require 'json'
+require 'Nokogiri'
+uri = "http://tktapi.melon.com/poc/ticketOpen/list.json?sortType=NEW&pageNo=1&v=1"
+result = JSON.parse(RestClient.get(uri).body)
 
-uri = "http://ticket.melon.com/csoon/detail.htm?csoonId=579"
-
-result = RestClient.get(uri)
-
-puts result.headers[:date]
-
-file = File.open("time.txt", "wb")
-file.write(result.headers[:date])
-file.close
-
+result["data"]["LIST"].each do |r|
+  puts r["csoonId"]
+  puts Time.parse(r["openDt"])
+  puts r["title"]
+  puts "http://cdnticket.melon.co.kr#{r["posterUrl"]}"
+  puts "http://ticket.melon.com/csoon/detail.htm?csoonId=#{r["csoonId"]}"
+  info = Nokogiri::HTML(r["infoPerf"])
+  puts info.text
+end
